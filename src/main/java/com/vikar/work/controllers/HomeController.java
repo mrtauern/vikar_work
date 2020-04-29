19fa09228e0ba5ee790221ac8f79d6e9cab03458
karatequin <gustavrodsgaard@gmail.com>
2020-04-29 10:28:01 +0200

    bootstrap design added to search button in header

--- src/main/java/com/vikar/work/controllers/HomeController.java
+++ src/main/java/com/vikar/work/controllers/HomeController.java
@@ -7,9 +7,7 @@ import com.vikar.work.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -164,5 +162,11 @@ public class HomeController {
         String user = type+""+id;
         session.setAttribute("login", user);
         log.info("login success");
+    }
+
+    @PostMapping("/search/{searchInput}")
+    public String search(@PathVariable("searchInput") String searchInput, @RequestParam("page") int page) {
+
+        return "redirect:/searchResult/"+searchInput+"/"+page;
     }
 }
